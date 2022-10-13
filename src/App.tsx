bdc86f8e6722dd7e4ac73738f8a5bc6b2272dbee
Michael Talalaev <talalaev.misha@gmail.com>
2022-10-13 18:05:54 +0300

    Change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+//This is component APP
+
+const App = () => {
+    return (
+        <div className="App">
+            <div>
+                <h1>Hi Div1</h1>
+            </div>
+            <h2> Div2</h2>
+            <ul>
+                Who here
+                <li>Hello</li>
+                <li>my</li>
+                <li>name</li>
+                <li>is</li>
+                <li>Mikhael</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
